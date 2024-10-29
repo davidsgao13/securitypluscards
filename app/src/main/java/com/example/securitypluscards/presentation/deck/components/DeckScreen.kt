567a41ba28f20a2aeb834519d72aedb98444591b
davidsgao13 <davidsgao13@gmail.com>
2024-10-28 21:12:59 -0400

    Started design on the DeckScreen

--- app/src/main/java/com/example/securitypluscards/presentation/deck/components/DeckScreen.kt
+++ app/src/main/java/com/example/securitypluscards/presentation/deck/components/DeckScreen.kt
@@ -1,11 +1,33 @@
 package com.example.securitypluscards.presentation.deck.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.example.securitypluscards.presentation.deck.view_models.DeckViewModel
 
 @Composable
-fun DeckScreen() {
-    val viewModel: DeckViewModel = hiltViewModel()
+fun DeckScreen(
+    navController: NavController,
+    viewModel: DeckViewModel = hiltViewModel()
+) {
     val state = viewModel.state.value
+
+    Box() {
+        // This should be the CardView later on
+        Row() {
+            Text(text = "Question")
+        }
+        // This should be the card number text
+        Text(text = "1/100")
+        Button(onClick = {}) {
+
+        }
+        Button(onClick = {}) {
+
+        }
+    }
 }
